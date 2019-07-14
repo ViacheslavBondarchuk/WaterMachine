7890f64951c49ffaadf1b9d6e45ebc471e799d14
Viacheslav <slava.777.bondarchuk@outlook.com>
2019-07-14 15:47:36 +0300

    was done validation

--- src/main/java/com/org/house/dto/TransactionDTO.java
+++ src/main/java/com/org/house/dto/TransactionDTO.java
@@ -1,30 +1,34 @@
 package com.org.house.dto;
 
-import lombok.Data;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import java.util.Date;
 
-@Data
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+import lombok.Getter;
+import lombok.Setter;
+import transfer.New;
+import transfer.Update;
+
+@Getter
+@Setter
 public class TransactionDTO {
-    @Null
-    private int id;
-    @NotNull
-    @NotEmpty
-    private int accountId;
-    @NotNull
-    @NotEmpty
-    private int automaticId;
-    private double cost;
-    @Null
-    private Date date;
-    @NotNull
-    @NotEmpty
-    private double quantityWater;
-    @NotNull
-    @NotEmpty
-    private boolean getMoney;
+
+	@Null(groups = { New.class })
+	@NotNull(groups = { Update.class })
+	private long id;
+	@NotBlank(groups = { New.class, Update.class })
+	private int accountId;
+	@NotBlank(groups = { New.class, Update.class })
+	private int automaticId;
+	@NotBlank(groups = { New.class, Update.class })
+	private double cost;
+	@Null(groups = { New.class, Update.class })
+	private Date date;
+	@NotBlank(groups = { New.class, Update.class })
+	private double quantityWater;
+	@NotBlank(groups = { New.class, Update.class })
+	private boolean getMoney;
 
 }
