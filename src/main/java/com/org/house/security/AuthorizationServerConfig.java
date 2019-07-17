d588a56e38e6d9681ce672933a570cfa5c6b36e4
Viacheslav <slava.777.bondarchuk@outlook.com>
2019-07-17 22:52:15 +0300

    was redone Ouath2

--- src/main/java/com/org/house/security/AuthorizationServerConfig.java
+++ src/main/java/com/org/house/security/AuthorizationServerConfig.java
@@ -1,75 +1,71 @@
-//package com.org.house.security;
-//
-//import com.org.house.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-//import org.springframework.security.oauth2.provider.ClientDetailsService;
-//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-//    private String clientid = "watermachineid";
-//    private String clientSecret = "MySecret";
-//    private String privateKey = "key";
-//    private String publiсKey = "key";
-//
-//    @Autowired
-//    private DataSource dataSource;
-//
-//    private UserService userService;
-//
-//    @Autowired
-//    @Qualifier("authenticationManagerBean")
-//    private AuthenticationManager authenticationManager;
-//
-//    @Bean
-//    public JwtAccessTokenConverter tokenConverter() {
-//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        converter.setSigningKey(privateKey);
-//        converter.setVerifierKey(publiсKey);
-//        return converter;
-//    }
-//
-//    @Bean
-//    public JwtTokenStore tokenStore() throws Exception {
-//        return new JwtTokenStore(tokenConverter());
-//    }
-//
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        endpoints
-//                .tokenStore(tokenStore())
-//                .accessTokenConverter(tokenConverter())
-//                .authenticationManager(authenticationManager);
-//    }
-//
-//    @Override
-//    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-//        security
-//                .tokenKeyAccess("permitAll()")
-//                .checkTokenAccess("isAuthenticated()");
-//    }
-//
-//    @Override
-//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        clients
-//               .withClientDetails((ClientDetailsService) userService)
-//                .withClient(clientid)
-//                .secret(clientSecret)
-//                .scopes("read", "write")
-//                .authorizedGrantTypes("password", "refresh_token")
-//                .accessTokenValiditySeconds(20000)
-//                .refreshTokenValiditySeconds(20000);
-//    }
-//
-//}
+package com.org.house.security;
+
+import com.org.house.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    static final String CLIEN_ID = "watermachineid";
+    static final String CLIENT_SECRET = "watermachinesecret";
+    static final String GRANT_TYPE_PASSWORD = "password";
+    static final String AUTHORIZATION_CODE = "authorization_code";
+    static final String REFRESH_TOKEN = "refresh_token";
+    static final String IMPLICIT = "implicit";
+    static final String SCOPE_READ = "read";
+    static final String SCOPE_WRITE = "write";
+    static final String TRUST = "trust";
+    static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1 * 60 * 60;
+    static final int FREFRESH_TOKEN_VALIDITY_SECONDS = 6 * 60 * 60;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Bean
+    public JwtAccessTokenConverter tokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("SigninKey");
+        return converter;
+
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(tokenConverter());
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .withClientDetails(clientDetailsService)
+                .withClient(CLIEN_ID)
+                .secret(CLIENT_SECRET)
+                .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT)
+                .scopes(SCOPE_READ, SCOPE_WRITE, TRUST)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS)
+                .refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager)
+                .accessTokenConverter(tokenConverter());
+    }
+}
--- src/main/java/com/org/house/security/WebSecurityConfig.java
+++ src/main/java/com/org/house/security/WebSecurityConfig.java
@@ -1,63 +1,64 @@
 package com.org.house.security;
 
 import com.org.house.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-//@Configuration
+import javax.annotation.Resource;
+
 @EnableWebSecurity
-//@Order(Ordered.HIGHEST_PRECEDENCE)
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UserService userService;
+    @Resource(name = "userService")
+    UserService userService;
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-
-
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-
-
-	@Override
-    protected void configure( final HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-				.antMatchers("/users","/companies","/login").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-				.logout().logoutRequestMatcher( new AntPathRequestMatcher("/logout")).permitAll();
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .anonymous().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.userDetailsService(userService)
-			.passwordEncoder(passwordEncoder());
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	}
-
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
